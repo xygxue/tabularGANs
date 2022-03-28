8c38c78c197e918626cad25bafdb41eeed7b93ad
Yuge Xue <88947362+xygxue@users.noreply.github.com>
2022-03-28 21:08:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+from datetime import date
+from pathlib import Path
+
+
+from CTAB_GAN.model.ctabgan import CTABGAN
+
+# Specifying the replication number
+num_exp = 1
+
+dataset = "czech_bank"
+real_datafile = 'trans_0.csv'
+real_path = os.path.join(Path(__file__).parent, 'src/main/resources/real_datasets', dataset, real_datafile)
+fake_path = os.path.join(Path(__file__).parent, 'src/main/resources/fake_datasets', dataset, 'fake_{exp}_{cur_date}.csv')
+
+
+synthesizer = CTABGAN(raw_csv_path=real_path,
+                      test_ratio=0.20,
+                      categorical_columns=['account_id', 'type', 'operation', 'k_symbol', 'bank', 'account'],
+                      log_columns=['amount', 'balance'],
+                      mixed_columns={'k_symbol': [0.0], 'bank': [0.0], 'account': [0.0]},
+                      integer_columns=['year', 'month', 'day', 'dayofweek'],
+                      problem_type={"Classification": 'operation'},
+                      epochs=100)
+
+
+if __name__ == '__main__':
+    # Fitting the synthesizer to the training dataset and generating synthetic data
+    today = date.today()
+    cur_date = today.strftime("%Y-%m-%d")
+
+    for i in range(num_exp):
+        synthesizer.fit()
+        syn = synthesizer.generate_samples()
+        syn.to_csv(fake_path.format(exp=i, cur_date=cur_date), index=False)
