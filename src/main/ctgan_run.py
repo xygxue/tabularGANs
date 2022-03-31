30a4b7fe4700836c9339bf4c29fb01c146a94af3
xygxue <yugexue@gmail.com>
2022-03-31 22:06:52 +0200

    add 1st fake data from ctgan

--- src/main/ctgan_run.py
+++ src/main/ctgan_run.py
@@ -1,15 +1,19 @@
 import os
+from datetime import date
 from pathlib import Path
 
 import pandas as pd
 from sdv.tabular import CTGAN
 
+exp = 3
 model_name = 'ctgan'
 dataset = "czech_bank"
 real_datafile = 'trans_3.csv'
 real_path = os.path.join(Path(__file__).parent, 'resources/real_datasets', dataset, real_datafile)
 fake_path = os.path.join(Path(__file__).parent, 'resources/fake_datasets', dataset,
                          '{model_name}_fake_{exp}_{cur_date}.csv')
+today = date.today()
+cur_date = today.strftime("%Y-%m-%d")
 
 real_data = pd.read_csv(real_path)
 real_data_len = real_data.shape[0]
@@ -19,3 +23,5 @@ model = CTGAN(rounding=0)
 model.fit(real_data)
 
 fake_data = model.sample(real_data_len)
+
+fake_data.to_csv(fake_path.format(model_name=model_name, exp=exp, cur_date=cur_date), index=False)
